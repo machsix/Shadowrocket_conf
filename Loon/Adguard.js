812836cf54115f3f90885f18cef2cb5b051039d8
machsix <28209092+machsix@users.noreply.github.com>
2023-12-19 01:33:43 -0500

    fix adguard

--- Loon/Adguard.js
+++ Loon/Adguard.js
@@ -1,11 +1,18 @@
-var body = $response.body;
-var obj = JSON.parse(body);
-obj = {
-  'products': [{
-    'product_id': "com.adguard.lifetimePurchase",
-    'premium_status': "ACTIVE"
-  }]
-};
+let body = $response.body;
+let obj = JSON.parse(body);
+const url = $request.url;
+if (url.includes("ios_validate_receipt")) {
+  obj = {
+    'products': [{
+      'product_id': "com.adguard.lifetimePurchase",
+      'premium_status': "ACTIVE"
+    }]
+  };
+} else {
+  obj.status = "PREMIUM";
+  obj.countryCode = "US";
+  obj.lifetime = "True";
+}
 body = JSON.stringify(obj);
 $done({
   'body': body
