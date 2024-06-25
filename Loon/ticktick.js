f6514d96749b61f7e7d640d42faae0b464ef2e9b
machsix <28209092+machsix@users.noreply.github.com>
2024-06-25 09:16:23 -0400

    Ticktick

---
+++ Loon/ticktick.js
@@ -0,0 +1,10 @@
+let obj = JSON.parse($response.body);
+
+obj.proEndDate = "3030-03-03T00:00:00.000+0000";
+obj.pro = true;
+obj.teamPro = true;
+obj.activeTeamUser = true;
+obj.teamUser = true;
+obj.freetrail = true;
+
+$done({ body: JSON.stringify(obj) });
